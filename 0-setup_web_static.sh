380a0b7837069b8c21889e164e71541e7781f0ba
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2020-04-12 14:40:16 -0500

    fixing up the code

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,36 +2,8 @@
 #This Bash script sets up your web servers for the deployment of web_static
 sudo apt-get update
 sudo apt-get install nginx
-if [ -d /data/ ];
-then
-  :
-else
-  sudo mkdir /data
-fi
-if [ -d /data/web_static ];
-then
-  :
-else
-  sudo mkdir /data/web_static/
-fi
-if [ -d /data/web_static/releases ];
-then
-  :
-else
-  sudo mkdir /data/web_static/releases
-fi
-if [ -d /data/web_static/shared ];
-then
-  :
-else
-  sudo mkdir /data/web_static/shared
-fi
-if [ -d /data/web_static/releases/test ];
-then
-  :
-else
-  sudo mkdir /data/web_static/releases/test
-fi
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
 echo "<html>
   <head>
   </head>
@@ -46,6 +18,6 @@ then
 else
   ln -s /data/web_static/releases/test /data/web_static/current
 fi
-sudo chown -R ubuntu:ubuntu /data/
+chown -R ubuntu:ubuntu /data/
 sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 sudo service nginx restart
