3011e4949628503be754e696c9ee78405fd2032a
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2020-04-22 15:52:53 -0500

    test the flask framework

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    strict_slashes = False
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    strict_slashes = False
+    return 'HBNB'
+
+@app.route('/c/<path:text>')
+def c(text):
+    strict_slashes = False
+    new = text.replace("_", " ")
+    return 'C %s' %new
+
+if __name__ == "__main__":
+    app.run()
